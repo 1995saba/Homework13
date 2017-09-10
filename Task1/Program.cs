eba9d265588ba30cade8f21b16c0918001e8dc25
Sabina <sabina.orunbaeva@gmail.com>
2017-09-10 08:01:30 +0600

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arrayFibonacciAsString=null;
+
+            try
+            {
+                using(StreamReader numbersReader = new StreamReader(File.Open("1.txt", FileMode.Open)))
+                {
+                    arrayFibonacciAsString += numbersReader.ReadToEnd();
+                }
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            string[] numbersAsString = arrayFibonacciAsString.Split(' ');
+            int size = numbersAsString.Length;
+            int[] arrayFibonacci = new int[size];
+            for(int i = 0; i < size; i++)
+            {
+                bool result = Int32.TryParse(numbersAsString[i], out arrayFibonacci[i]);
+                if (!result)
+                {
+                    Console.WriteLine("Attempted conversion of '{0}' failed.",
+                                   numbersAsString[i] == null ? "<null>" : numbersAsString[i]);
+                }
+            }
+
+            int[] arrayFibonacciSecondPart = new int[size];
+            arrayFibonacciSecondPart[0] = arrayFibonacci[size - 1] + arrayFibonacci[size - 2];
+            arrayFibonacciSecondPart[1] = arrayFibonacciSecondPart[0] + arrayFibonacci[size - 1];
+
+            for(int i = 2; i < size; i++)
+            {
+                arrayFibonacciSecondPart[i] = arrayFibonacciSecondPart[i - 1] + arrayFibonacciSecondPart[i - 2];
+            }
+
+            try
+            {
+                using (StreamWriter numbersWriter = new StreamWriter(File.Open("1.txt", FileMode.Append)))
+                {
+                    for(int i = 0; i < size; i++)
+                    {
+                        numbersWriter.Write(" ");
+                        numbersWriter.Write(arrayFibonacciSecondPart[i]);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string abAsString = null;
+            try
+            {
+                using (StreamReader reader = new StreamReader(File.Open("INPUT.txt", FileMode.Open)))
+                {
+                    abAsString += reader.ReadToEnd();
+                }
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            string[] aAndBAsString = abAsString.Split(' ');
+            int numA, numB;
+            bool result = Int32.TryParse(aAndBAsString[0], out numA);
+            if (!result)
+            {
+                Console.WriteLine("Attempted conversion of '{0}' failed.",
+                               aAndBAsString[0] == null ? "<null>" : aAndBAsString[0]);
+            }
+
+            result = Int32.TryParse(aAndBAsString[1], out numB);
+            if (!result)
+            {
+                Console.WriteLine("Attempted conversion of '{0}' failed.",
+                               aAndBAsString[1] == null ? "<null>" : aAndBAsString[1]);
+            }
+
+            int num = numA + numB;
+
+            try
+            {
+                using (StreamWriter numbersWriter = new StreamWriter(File.Open("OUTPUT.txt", FileMode.OpenOrCreate)))
+                {
+                    numbersWriter.Write(num);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
